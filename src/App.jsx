40f0f830b6f79cf6f59ed28d45cbccbe6056c697
Chibueze Obioma <justixxchiobi@gmail.com>
2026-08-21 16:55:34 +0100

    Add cinematic fade transitions between all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import { useNavigate } from 'react-router-dom'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
+import { AnimatePresence, motion } from 'framer-motion'
 import { CartProvider } from './context/CartContext'
+import { WishlistProvider } from './context/WishlistContext'
+import { AuthProvider } from './context/AuthContext'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import ProductPage from './pages/ProductPage'
+import WhatsAppButton from './components/WhatsAppButton'
 import Home from './pages/Home'
 import Shop from './pages/Shop'
+import ProductPage from './pages/ProductPage'
+import CategoryPage from './pages/CategoryPage'
 import Cart from './pages/Cart'
-import { WishlistProvider } from './context/WishlistContext'
 import Wishlist from './pages/Wishlist'
 import About from './pages/About'
-import CategoryPage from './pages/CategoryPage'
-import NotFound from './pages/NotFound'
 import Contact from './pages/Contact'
 import FAQ from './pages/FAQ'
 import Delivery from './pages/Delivery'
@@ -20,11 +21,48 @@ import Lookbook from './pages/Lookbook'
 import Journal from './pages/Journal'
 import SourceRequest from './pages/SourceRequest'
 import Checkout from './pages/Checkout'
-import { AuthProvider } from './context/AuthContext'
 import Account from './pages/Account'
 import OrderHistory from './pages/OrderHistory'
 import Admin from './pages/Admin'
-import WhatsAppButton from './components/WhatsAppButton'
+import NotFound from './pages/NotFound'
+
+function AnimatedRoutes() {
+  const location = useLocation()
+
+  return (
+    <AnimatePresence mode="wait" initial={false}>
+      <motion.div
+        key={location.pathname}
+        initial={{ opacity: 0, y: 8 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: -8 }}
+        transition={{ duration: 0.35, ease: [0.16, 1, 0.3, 1] }}
+      >
+        <Routes location={location}>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/product/:id" element={<ProductPage />} />
+          <Route path="/category/:categoryId" element={<CategoryPage />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/wishlist" element={<Wishlist />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/delivery" element={<Delivery />} />
+          <Route path="/returns" element={<Returns />} />
+          <Route path="/lookbook" element={<Lookbook />} />
+          <Route path="/journal" element={<Journal />} />
+          <Route path="/source" element={<SourceRequest />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/orders" element={<OrderHistory />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </motion.div>
+    </AnimatePresence>
+  )
+}
 
 function App() {
   return (
@@ -34,28 +72,7 @@ function App() {
           <WishlistProvider>
             <div className="min-h-screen bg-cream dark:bg-espresso transition-colors">
               <Navbar />
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/shop" element={<Shop />} />
-                <Route path="/product/:id" element={<ProductPage />} />
-                <Route path="/cart" element={<Cart />} />
-                <Route path="/wishlist" element={<Wishlist />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/lookbook" element={<Lookbook />} />
-                <Route path="/category/:categoryId" element={<CategoryPage />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route path="/journal" element={<Journal />} />
-                <Route path="/source" element={<SourceRequest />} />
-                <Route path="/checkout" element={<Checkout />} />
-                <Route path="/account" element={<Account />} />
-                <Route path="/orders" element={<OrderHistory />} />
-                <Route path="/admin" element={<Admin />} />
-
-                <Route path="/faq" element={<FAQ />} />
-                <Route path="/delivery" element={<Delivery />} />
-                <Route path="/returns" element={<Returns />} />
-                <Route path="*" element={<NotFound />} />
-              </Routes>
+              <AnimatedRoutes />
               <Footer />
               <WhatsAppButton />
             </div>
